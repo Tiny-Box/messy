80336292604ec20fed768ce7e0b00e29163fa050
TinyBox <Tiny_Box@outlook.com>
2014-08-14 20:11:11 +0800

    modify

--- kf_sql/kf_sql/MainWindow.xaml.cs
+++ kf_sql/kf_sql/MainWindow.xaml.cs
@@ -12,17 +12,405 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.OleDb;
+using System.Collections;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace kf_sql
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #region 打开Excel
+        public void OpenExcel(object obj, RoutedEventArgs e)
+        {
+            conExcel("a");
+            //Stream myStream = null;
+            //Microsoft.Win32.OpenFileDialog openFileDialog1 = new Microsoft.Win32.OpenFileDialog();
+
+            //openFileDialog1.InitialDirectory = "c:\\";
+            //openFileDialog1.Filter = "Excel files (*.xls, *xlsx)|*.xls; *xlsx";
+            //openFileDialog1.FilterIndex = 1;
+            //openFileDialog1.RestoreDirectory = true;
+
+            //bool? userClickedOK = openFileDialog1.ShowDialog();
+
+            //if (userClickedOK == true)
+            //{
+            //    try
+            //    {
+            //        if ((myStream = openFileDialog1.OpenFile()) != null)
+            //        {
+            //            using (myStream)
+            //            {
+            //                //string ConnectionStr = "";
+            //                ////ConnectionStr = this.judgeVersion(openFileDialog1.FileName);
+
+            //                ////MessageBox.Show(ConnectionStr);
+            //                ////this.conExcel(ConnectionStr);
+            //                ////MessageBox.Show(PubCom.Ylabel);
+            //                //MessageBox.Show(openFileDialog1.FileName);
+            //                conExcel(openFileDialog1.FileName);
+
+            //            }
+
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+            //    }
+            //}
+        }
+        #endregion
+
+        #region 给Excel连接字符串
+        private string conExcel_str(string filename)
+        {
+            return "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filename + ";" +
+                                "Extended Properties='Excel 12.0 Xml;HDR=NO" + ";'";
+        }
+        #endregion
+
+        // 生成连接SQL
+        public string ConnectionStr(string Database, string DataSource, string User, string Password)
+        {
+            return "Database='" + Database + "';Data Source='" + DataSource + "';User Id='" + User + "';Password='" + Password + "';charset='utf8';pooling=true";
+        }
+
+        public void conExcel(string filename)
+        {
+            filename = "D:/Book1.xlsx";
+            //string connection = conExcel_str(filename);
+
+            DataSet dtTemp = new DataSet();
+
+            //OleDbConnection objConn = new OleDbConnection(connection);
+            //objConn.Open();
+
+            //OleDbDataAdapter oda = new OleDbDataAdapter("select * from [Sheet1$]", objConn);
+
+            //oda.Fill(dtTemp);
+            object oMissing = System.Reflection.Missing.Value;
+
+            Excel.Application xlApp = new Excel.Application();
+
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(filename, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing);
+            
+            Excel.Worksheet ws = (Excel.Worksheet)xlWorkbook.Worksheets[1];
+
+            //服务器数量
+            int num = ws.UsedRange.CurrentRegion.Rows.Count;
+
+            //最低级别
+            Excel.Range rRng = xlApp.ActiveCell;
+            rRng = ws.get_Range("E2", "E16");
+            object[,] rank = rRng.Value;
+
+            //开服时间
+            rRng = ws.get_Range("G2", "G16");
+            object[,] time = rRng.Value;
+
+            DateTime test = DateTime.Parse(time[1, 1].ToString());
+            DateTime test1 = test.AddSeconds(1);
+
+            string conn = "Database='test_cs';Data Source='localhost';User Id='root';Password='1234';charset='utf8';pooling=true";
+
+            string drop = "DROP TABLE IF EXISTS `kfgz_season_info`;";
+
+            string addTable = "CREATE TABLE `kfgz_season_info` (`pk` int(11) NOT NULL AUTO_INCREMENT, `season_id` int(11) DEFAULT NULL, `state` int(1) DEFAULT NULL, `rule_id` int(11) DEFAULT NULL, `rewardg_id` int(11) DEFAULT NULL, `begin_time` datetime DEFAULT NULL, `first_battle_time` datetime DEFAULT NULL, `end_time` datetime DEFAULT NULL, `game_server_limit` int(1) DEFAULT '0', PRIMARY KEY(`pk`)) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;";
+
+            int b = ExecuteNonQuery(conn, CommandType.Text, drop, null);
+            MessageBox.Show(b.ToString());
+            int c = ExecuteNonQuery(conn, CommandType.Text, addTable, null);
+            MessageBox.Show(c.ToString());
+
+            
+
+            
+
+
+
+            xlApp.Visible = false;
+
+            xlApp.Quit();
+
+        }
+
+
+        public string[] kfwd_rule_sql(int num, object[,] rank, object[,] time)
+        {
+            string[] sql = new string[num];
+
+            return sql;
+        }
+
+        #region SQL 操作
+        // 用于缓存参数的HASH表
+        private static Hashtable parmCache = Hashtable.Synchronized(new Hashtable());
+
+        /// <summary>
+        ///  给定连接的数据库用假设参数执行一个sql命令（不返回数据集）
+        /// </summary>
+        /// <param name="connectionString">一个有效的连接字符串</param>
+        /// <param name="cmdType">命令类型(存储过程, 文本, 等等)</param>
+        /// <param name="cmdText">存储过程名称或者sql命令语句</param>
+        /// <param name="commandParameters">执行命令所用参数的集合</param>
+        /// <returns>执行命令所影响的行数</returns>
+        public static int ExecuteNonQuery(string connectionString, CommandType cmdType, string cmdText, params MySqlParameter[] commandParameters)
+        {
+
+            MySqlCommand cmd = new MySqlCommand();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameters);
+                int val = cmd.ExecuteNonQuery();
+                cmd.Parameters.Clear();
+                return val;
+            }
+        }
+
+        
+        /// <summary>
+        /// 用现有的数据库连接执行一个sql命令（不返回数据集）
+        /// </summary>
+        /// <param name="connection">一个现有的数据库连接</param>
+        /// <param name="cmdType">命令类型(存储过程, 文本, 等等)</param>
+        /// <param name="cmdText">存储过程名称或者sql命令语句</param>
+        /// <param name="commandParameters">执行命令所用参数的集合</param>
+        /// <returns>执行命令所影响的行数</returns>
+        public static int ExecuteNonQuery(MySqlConnection connection, CommandType cmdType, string cmdText, params MySqlParameter[] commandParameters)
+        {
+
+            MySqlCommand cmd = new MySqlCommand();
+
+            PrepareCommand(cmd, connection, null, cmdType, cmdText, commandParameters);
+            int val = cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            return val;
+        }
+
+        /// <summary>
+        ///使用现有的SQL事务执行一个sql命令（不返回数据集）
+        /// </summary>
+        /// <remarks>
+        ///举例:
+        ///  int result = ExecuteNonQuery(connString, CommandType.StoredProcedure, "PublishOrders", new MySqlParameter("@prodid", 24));
+        /// </remarks>
+        /// <param name="trans">一个现有的事务</param>
+        /// <param name="cmdType">命令类型(存储过程, 文本, 等等)</param>
+        /// <param name="cmdText">存储过程名称或者sql命令语句</param>
+        /// <param name="commandParameters">执行命令所用参数的集合</param>
+        /// <returns>执行命令所影响的行数</returns>
+        public static int ExecuteNonQuery(MySqlTransaction trans, CommandType cmdType, string cmdText, params MySqlParameter[] commandParameters)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            PrepareCommand(cmd, trans.Connection, trans, cmdType, cmdText, commandParameters);
+            int val = cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            return val;
+        }
+
+        /// <summary>
+        /// 用执行的数据库连接执行一个返回数据集的sql命令
+        /// </summary>
+        /// <remarks>
+        /// 举例:
+        ///  MySqlDataReader r = ExecuteReader(connString, CommandType.StoredProcedure, "PublishOrders", new MySqlParameter("@prodid", 24));
+        /// </remarks>
+        /// <param name="connectionString">一个有效的连接字符串</param>
+        /// <param name="cmdType">命令类型(存储过程, 文本, 等等)</param>
+        /// <param name="cmdText">存储过程名称或者sql命令语句</param>
+        /// <param name="commandParameters">执行命令所用参数的集合</param>
+        /// <returns>包含结果的读取器</returns>
+        public static MySqlDataReader ExecuteReader(string connectionString, CommandType cmdType, string cmdText, params MySqlParameter[] commandParameters)
+        {
+            //创建一个MySqlCommand对象
+            MySqlCommand cmd = new MySqlCommand();
+            //创建一个MySqlConnection对象
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            //在这里我们用一个try/catch结构执行sql文本命令/存储过程，因为如果这个方法产生一个异常我们要关闭连接，因为没有读取器存在，
+            //因此commandBehaviour.CloseConnection 就不会执行
+            try
+            {
+                //调用 PrepareCommand 方法，对 MySqlCommand 对象设置参数
+                PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameters);
+                //调用 MySqlCommand  的 ExecuteReader 方法
+                MySqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                //清除参数
+                cmd.Parameters.Clear();
+                return reader;
+            }
+            catch
+            {
+                //关闭连接，抛出异常
+                conn.Close();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 返回DataSet
+        /// </summary>
+        /// <param name="connectionString">一个有效的连接字符串</param>
+        /// <param name="cmdType">命令类型(存储过程, 文本, 等等)</param>
+        /// <param name="cmdText">存储过程名称或者sql命令语句</param>
+        /// <param name="commandParameters">执行命令所用参数的集合</param>
+        /// <returns></returns>
+        public static DataSet GetDataSet(string connectionString, CommandType cmdType, string cmdText, params MySqlParameter[] commandParameters)
+        {
+            //创建一个MySqlCommand对象
+            MySqlCommand cmd = new MySqlCommand();
+            //创建一个MySqlConnection对象
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            //在这里我们用一个try/catch结构执行sql文本命令/存储过程，因为如果这个方法产生一个异常我们要关闭连接，因为没有读取器存在，
+
+            try
+            {
+                //调用 PrepareCommand 方法，对 MySqlCommand 对象设置参数
+                PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameters);
+                //调用 MySqlCommand  的 ExecuteReader 方法
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataSet ds = new DataSet();
+
+                adapter.Fill(ds);
+                //清除参数
+                cmd.Parameters.Clear();
+                conn.Close();
+                return ds;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+
+
+        /// <summary>
+        /// 用指定的数据库连接字符串执行一个命令并返回一个数据集的第一列
+        /// </summary>
+        /// <remarks>
+        ///例如:
+        ///  Object obj = ExecuteScalar(connString, CommandType.StoredProcedure, "PublishOrders", new MySqlParameter("@prodid", 24));
+        /// </remarks>
+        ///<param name="connectionString">一个有效的连接字符串</param>
+        /// <param name="cmdType">命令类型(存储过程, 文本, 等等)</param>
+        /// <param name="cmdText">存储过程名称或者sql命令语句</param>
+        /// <param name="commandParameters">执行命令所用参数的集合</param>
+        /// <returns>用 Convert.To{Type}把类型转换为想要的 </returns>
+        public static object ExecuteScalar(string connectionString, CommandType cmdType, string cmdText, params MySqlParameter[] commandParameters)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                PrepareCommand(cmd, connection, null, cmdType, cmdText, commandParameters);
+                object val = cmd.ExecuteScalar();
+                cmd.Parameters.Clear();
+                return val;
+            }
+        }
+
+        /// <summary>
+        /// 用指定的数据库连接执行一个命令并返回一个数据集的第一列
+        /// </summary>
+        /// <remarks>
+        /// 例如:
+        ///  Object obj = ExecuteScalar(connString, CommandType.StoredProcedure, "PublishOrders", new MySqlParameter("@prodid", 24));
+        /// </remarks>
+        /// <param name="connection">一个存在的数据库连接</param>
+        /// <param name="cmdType">命令类型(存储过程, 文本, 等等)</param>
+        /// <param name="cmdText">存储过程名称或者sql命令语句</param>
+        /// <param name="commandParameters">执行命令所用参数的集合</param>
+        /// <returns>用 Convert.To{Type}把类型转换为想要的 </returns>
+        public static object ExecuteScalar(MySqlConnection connection, CommandType cmdType, string cmdText, params MySqlParameter[] commandParameters)
+        {
+
+            MySqlCommand cmd = new MySqlCommand();
+
+            PrepareCommand(cmd, connection, null, cmdType, cmdText, commandParameters);
+            object val = cmd.ExecuteScalar();
+            cmd.Parameters.Clear();
+            return val;
+        }
+
+        /// <summary>
+        /// 将参数集合添加到缓存
+        /// </summary>
+        /// <param name="cacheKey">添加到缓存的变量</param>
+        /// <param name="commandParameters">一个将要添加到缓存的sql参数集合</param>
+        public static void CacheParameters(string cacheKey, params MySqlParameter[] commandParameters)
+        {
+            parmCache[cacheKey] = commandParameters;
+        }
+
+        /// <summary>
+        /// 找回缓存参数集合
+        /// </summary>
+        /// <param name="cacheKey">用于找回参数的关键字</param>
+        /// <returns>缓存的参数集合</returns>
+        public static MySqlParameter[] GetCachedParameters(string cacheKey)
+        {
+            MySqlParameter[] cachedParms = (MySqlParameter[])parmCache[cacheKey];
+
+            if (cachedParms == null)
+                return null;
+
+            MySqlParameter[] clonedParms = new MySqlParameter[cachedParms.Length];
+
+            for (int i = 0, j = cachedParms.Length; i < j; i++)
+                clonedParms[i] = (MySqlParameter)((ICloneable)cachedParms[i]).Clone();
+
+            return clonedParms;
+        }
+
+        /// <summary>
+        /// 准备执行一个命令
+        /// </summary>
+        /// <param name="cmd">sql命令</param>
+        /// <param name="conn">OleDb连接</param>
+        /// <param name="trans">OleDb事务</param>
+        /// <param name="cmdType">命令类型例如 存储过程或者文本</param>
+        /// <param name="cmdText">命令文本,例如:Select * from Products</param>
+        /// <param name="cmdParms">执行命令的参数</param>
+        private static void PrepareCommand(MySqlCommand cmd, MySqlConnection conn, MySqlTransaction trans, CommandType cmdType, string cmdText, MySqlParameter[] cmdParms)
+        {
+
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+
+            if (trans != null)
+                cmd.Transaction = trans;
+
+            cmd.CommandType = cmdType;
+
+            if (cmdParms != null)
+            {
+                foreach (MySqlParameter parm in cmdParms)
+                    cmd.Parameters.Add(parm);
+            }
+        }
+        #endregion
+
+
     }
 }
